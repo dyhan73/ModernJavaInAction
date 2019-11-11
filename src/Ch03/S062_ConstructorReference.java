0a55ac7955a8acbd7e0db255f6ade2eac93cf200
한대영 <dyhan@sk.com>
2019-11-11 15:35:22 +0900

    3.6.2 Constructor reference - using BiFunction for 2 arguments (Color, weight)

--- src/Ch03/S062_ConstructorReference.java
+++ src/Ch03/S062_ConstructorReference.java
@@ -3,6 +3,7 @@ package Ch03;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class S062_ConstructorReference {
@@ -36,8 +37,19 @@ public class S062_ConstructorReference {
         }
         return result;
     }
+
+    List<Color> colors = Arrays.asList(Color.RED, Color.RED, Color.GREEN, Color.GREEN);
+    List<Apple> apples2 = map2(colors, weights, Apple::new);
+    public List<Apple> map2(List<Color> cList, List<Integer> wList, BiFunction<Color, Integer, Apple> f) {
+        List<Apple> result = new ArrayList<>();
+        for (int i=0; i<cList.size(); i++) {
+            result.add(f.apply(cList.get(i), wList.get(i)));
+        }
+        return result;
+    }
     public static void main(String[] args) {
         S062_ConstructorReference proc = new S062_ConstructorReference();
         System.out.println(proc.apples);
+        System.out.println(proc.apples2);
     }
 }
