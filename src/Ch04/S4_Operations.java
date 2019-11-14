50ea39f317933f98b9b2e26725ffb12127988797
한대영 <dyhan@sk.com>
2019-11-14 21:31:16 +0900

    4-4 Stream operations

---
+++ src/Ch04/S4_Operations.java
@@ -0,0 +1,40 @@
+package Ch04;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class S4_Operations {
+    public static void main(String[] args) {
+        // Intermediate operation
+        List<Dish> menu = Dish.getMenu();
+
+        List<String> names =
+                menu.stream()
+                .filter(dish -> {
+                    System.out.println("filtering:" + dish.getName());
+                    return dish.getCalories() > 300;
+                })
+                .map(dish -> {
+                    System.out.println("mapping:" + dish.getName());
+                    return dish.getName();
+                })
+                .limit(3)
+                .collect(Collectors.toList());
+        System.out.println(names);
+
+        /* result (lazy calculation)
+            filtering:pork
+            mapping:pork
+            filtering:beef
+            mapping:beef
+            filtering:chicken
+            mapping:chicken
+            [pork, beef, chicken]
+         */
+
+        // Terminal operation
+        menu.stream().forEach(System.out::println);
+
+        // Quiz 4-2
+    }
+}
