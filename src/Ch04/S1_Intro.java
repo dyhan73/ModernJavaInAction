feb416aaca956591180aa1359ee8f8163576e2ca
한대영 <dyhan@sk.com>
2019-11-14 20:39:21 +0900

    4-2 filter, map, limit and collect

--- src/Ch04/S1_Intro.java
+++ src/Ch04/S1_Intro.java
@@ -2,10 +2,20 @@ package Ch04;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class S1_Intro {
     public static void main(String[] args) {
         List<Dish> menu = Dish.getMenu();
         System.out.println(menu);
+
+        // 4-2
+        List<String> threeHighCaloricDishNames =
+                menu.stream()
+                .filter(dish -> dish.getCalories() > 300)
+                .map(Dish::getName)
+                .limit(3)
+                .collect(Collectors.toList());
+        System.out.println(threeHighCaloricDishNames);
     }
 }
