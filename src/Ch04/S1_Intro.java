40fd5a01976170110c8290334d3c0df3ed349ffd
한대영 <dyhan@sk.com>
2019-11-14 11:13:15 +0900

    4.1 Stream Intro - Dish class

---
+++ src/Ch04/S1_Intro.java
@@ -0,0 +1,11 @@
+package Ch04;
+
+import java.util.List;
+import java.util.Map;
+
+public class S1_Intro {
+    public static void main(String[] args) {
+        List<Dish> menu = Dish.getMenu();
+        System.out.println(menu);
+    }
+}
