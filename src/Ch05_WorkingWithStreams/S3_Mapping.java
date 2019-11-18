80aefccea94e8bc9eb2b61196717ba63ecc6d812
한대영 <dyhan@sk.com>
2019-11-18 17:05:38 +0900

    5-3-2 Map & flatMap

--- src/Ch05_WorkingWithStreams/S3_Mapping.java
+++ src/Ch05_WorkingWithStreams/S3_Mapping.java
@@ -5,10 +5,12 @@ import Ch04_IntroducingStreams.Dish;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class S3_Mapping {
     public static void main(String[] args) {
         S531_Intro();
+        S532_FlatMap();
     }
 
     private static void S531_Intro() {
@@ -32,5 +34,37 @@ public class S3_Mapping {
                 .map(String::length)
                 .collect(Collectors.toList());
         System.out.println(menuNameLength);
+    }
+
+    private static void S532_FlatMap() {
+        // using split (not working because split return type is String[]
+        List<String> words = Arrays.asList("Hello", "World");
+        List<String[]> chars = words.stream()
+                .map(w -> w.split(""))
+                .distinct()
+                .collect(Collectors.toList());
+        for (String[] sa : chars) {
+            for (String s : sa)
+            System.out.println(s);
+        }
+
+        // using map & Arrays.stream
+        String[] arrayOfWord = {"Goodbye", "World"};
+        Stream<String> streamOfWord = Arrays.stream(arrayOfWord);
+        System.out.println(streamOfWord.collect(Collectors.joining()));
+
+        List<Stream<String>> chars2 = words.stream()
+                .map(word -> word.split(""))
+                .map(Arrays::stream)
+                .distinct()
+                .collect(Collectors.toList());
+
+        // using flatMap (Flatting nested list)
+        List<String> uniqueCharacters = words.stream()
+                .map(word -> word.split(""))
+                .flatMap(Arrays::stream)
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println(uniqueCharacters);
     }
 }
