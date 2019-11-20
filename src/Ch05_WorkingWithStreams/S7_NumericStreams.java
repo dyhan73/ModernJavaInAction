07b773e3ee967081fa7c96c87d5428505ab0c345
한대영 <dyhan@sk.com>
2019-11-20 15:51:08 +0900

    5-7 Numeric Stream Specialization

--- src/Ch05_WorkingWithStreams/S7_NumericStreams.java
+++ src/Ch05_WorkingWithStreams/S7_NumericStreams.java
@@ -14,7 +14,8 @@ public class S7_NumericStreams {
 
         s570_Intro(menu);
         s571_NumericSpecialization(menu);
-
+        s572_NumericRanges();
+        s573_Practice_PythagoreanTriples();
     }
 
     private static void s570_Intro(List<Dish> menu) {
@@ -52,5 +53,39 @@ public class S7_NumericStreams {
                 .mapToInt(Dish::getCalories)
                 .max();
         maxCalories2.ifPresent(System.out::println); // not presented
+    }
+
+    private static void s572_NumericRanges() {
+        // .range(s, f) => s, s+1, ... f-1
+        // .rangeClosed(s, f) => s, s+1, ... f-1, f
+        IntStream evenNumbers = IntStream.range(1, 100)
+                .filter(n -> n % 2 == 1);
+//        System.out.println(evenNumbers.count());
+        System.out.println(evenNumbers.min());
+//        System.out.println(evenNumbers.max());
+    }
+
+    private static void s573_Practice_PythagoreanTriples() {
+        // Pythagorean Triples : a^2 + b^2 = c^2
+        Stream<int[]> pythagoreanTriples =
+                IntStream.rangeClosed(1, 100).boxed()
+                .flatMap(a ->
+                        IntStream.rangeClosed(a, 100)
+                        .filter(b -> Math.sqrt(a*a + b*b) % 1 == 0)
+                        .mapToObj(b -> new int[]{a, b, (int)Math.sqrt(a*a + b*b)})
+        );
+        pythagoreanTriples.limit(5)
+                .forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+
+        // enhancement
+        // pros: reduce sqrt calculation
+        // cons: must using double[]
+        Stream<double[]> pythagoreanTriples2 =
+                IntStream.rangeClosed(1, 100).boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                    .mapToObj(b -> new double[]{a, b, Math.sqrt(a*a + b*b)})
+                    .filter(t -> t[2] % 1 == 0));
+        pythagoreanTriples2.limit(5)
+                .forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
     }
 }
