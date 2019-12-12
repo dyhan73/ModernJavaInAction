5c07815a8158e734cfd053052717b550a482f36b
한대영 <dyhan@sk.com>
2019-12-12 10:58:45 +0900

    Section 9.3.2 wrapping lambda with class method - override equals() is needed to comparing

--- src/Ch09_RefactorTestDebug/S3_LambdaTesting.java
+++ src/Ch09_RefactorTestDebug/S3_LambdaTesting.java
@@ -1,6 +1,7 @@
 package Ch09_RefactorTestDebug;
 
-import java.util.Comparator;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class S3_LambdaTesting {
     static class Point {
@@ -25,6 +26,33 @@ public class S3_LambdaTesting {
         }
 
         // static variable of lambda
-        public final static Comparator<Point> compareByXAndThenY = Comparator.comparing(Point::getX).thenComparing(Point::getY);
+        public final static Comparator<Point> compareByXAndThenY =
+                Comparator.comparing(Point::getX).thenComparing(Point::getY);
+
+        // wrapping lambda as (static) method
+        public static List<Point> moveAllPointsRightBy(List<Point> points, int x) {
+            return points.stream()
+                    .map(p -> new Point(p.getX() + x, p.getY()))
+                    .collect(Collectors.toList());
+        }
+
+        // equals must override to avoid using Object's default equals()
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Point)) return false;
+
+            Point point = (Point) o;
+
+            if (x != point.x) return false;
+            return y == point.y;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = x;
+            result = 31 * result + y;
+            return result;
+        }
     }
 }
--- test/Ch09_RefactorTestDebug/S3_LambdaTestingTest.java
+++ test/Ch09_RefactorTestDebug/S3_LambdaTestingTest.java
@@ -1,25 +1,32 @@
 package Ch09_RefactorTestDebug;
 
+import Ch09_RefactorTestDebug.S3_LambdaTesting.Point;
+import java.util.*;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class S3_LambdaTestingTest {
     @Test
     public void testMoveRightBy() {
-        S3_LambdaTesting.Point p1 = new S3_LambdaTesting.Point(5, 5);
-        S3_LambdaTesting.Point p2 = p1.moveRightBy(10);
+        Point p1 = new Point(5, 5);
+        Point p2 = p1.moveRightBy(10);
         assertEquals(15, p2.getX());
         assertEquals(5, p2.getY());
     }
 
     @Test
     public void testComparingTwoPoints() throws Exception {
-        S3_LambdaTesting.Point p1 = new S3_LambdaTesting.Point(10, 15);
-        S3_LambdaTesting.Point p2 = new S3_LambdaTesting.Point(10, 20);
-        int result = S3_LambdaTesting.Point.compareByXAndThenY.compare(p1, p2);
+        Point p1 = new Point(10, 15);
+        Point p2 = new Point(10, 20);
+        int result = Point.compareByXAndThenY.compare(p1, p2);
         assertTrue(result < 0);
     }
 
+    @Test
+    public void testMoveAllPointsRightBy() throws Exception {
+        List<Point> points = Arrays.asList(new Point(5, 5), new Point(10, 5));
+        List<Point> expectedPoints = Arrays.asList(new Point(15, 5), new Point(20, 5));
+        List<Point> newPoints = Point.moveAllPointsRightBy(points, 10);
+        assertEquals(expectedPoints, newPoints);
+    }
 }
