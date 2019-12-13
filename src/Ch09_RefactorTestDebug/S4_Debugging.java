325d915b76d10f972d24fec3409a83d64ea9f571
한대영 <dyhan@sk.com>
2019-12-13 16:01:12 +0900

    Section 9.4.2 using Stream.peek() to check value without consuming

--- src/Ch09_RefactorTestDebug/S4_Debugging.java
+++ src/Ch09_RefactorTestDebug/S4_Debugging.java
@@ -2,10 +2,12 @@ package Ch09_RefactorTestDebug;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class S4_Debugging {
     public static void main(String[] args) {
         s941_checkStackTrace();
+        s944_usingPeekOfStream();
     }
 
     private static void s941_checkStackTrace() {
@@ -31,6 +33,27 @@ public class S4_Debugging {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void s944_usingPeekOfStream() {
+        List<Integer> numbers = Arrays.asList(2, 3, 4, 5);
+        numbers.stream()
+                .map(x -> x + 17)
+                .filter(x -> x % 2 == 0)
+                .limit(3)
+                .forEach(System.out::println);
+
+        // using peek() to check without consume the stream
+        List<Integer> result = numbers.stream()
+                .peek(x -> System.out.println("from stream : " + x))
+                .map(x -> x + 17)
+                .peek(x -> System.out.println("after map : " + x))
+                .filter(x -> x % 2 == 0)
+                .peek(x -> System.out.println("after filter : " + x))
+                .limit(3)
+                .peek(x -> System.out.println("after limit : " + x))
+                .collect(Collectors.toList());
+        System.out.println(result);
     }
 
     public static int divideByZero(int n) {
