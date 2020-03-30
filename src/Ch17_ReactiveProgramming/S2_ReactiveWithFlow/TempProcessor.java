b2cb6155985f81bd7850f7fd035841a84530c7dd
한대영 <dyhan@sk.com>
2020-03-30 20:45:00 +0900

    Ch17.2.3 Processor to convert temperature unit

---
+++ src/Ch17_ReactiveProgramming/S2_ReactiveWithFlow/TempProcessor.java
@@ -0,0 +1,33 @@
+package Ch17_ReactiveProgramming.S2_ReactiveWithFlow;
+
+import java.util.concurrent.Flow;
+
+public class TempProcessor implements Flow.Processor<TempInfo, TempInfo> {
+
+    private Flow.Subscriber<? super TempInfo> subscriber;
+
+    @Override
+    public void subscribe(Flow.Subscriber<? super TempInfo> subscriber) {
+        this.subscriber = subscriber;
+    }
+
+    @Override
+    public void onSubscribe(Flow.Subscription subscription) {
+        subscriber.onSubscribe(subscription);
+    }
+
+    @Override
+    public void onNext(TempInfo item) {
+        subscriber.onNext(new TempInfo(item.getTown(), (item.getTemp() - 32) * 5 / 9));
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        subscriber.onError(throwable);
+    }
+
+    @Override
+    public void onComplete() {
+        subscriber.onComplete();
+    }
+}
