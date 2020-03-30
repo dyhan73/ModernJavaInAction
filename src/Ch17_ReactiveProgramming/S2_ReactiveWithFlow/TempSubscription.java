8c1340347b196e8abccf26f378ccf71c512b8ff7
한대영 <dyhan@sk.com>
2020-03-30 20:30:23 +0900

    Ch17.2.2 using executor to avoid stack overflow

--- src/Ch17_ReactiveProgramming/S2_ReactiveWithFlow/TempSubscription.java
+++ src/Ch17_ReactiveProgramming/S2_ReactiveWithFlow/TempSubscription.java
@@ -1,11 +1,15 @@
 package Ch17_ReactiveProgramming.S2_ReactiveWithFlow;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Flow;
 
 public class TempSubscription implements Flow.Subscription {
 
     private final Flow.Subscriber<? super TempInfo> subscriber;
     private final String town;
+
+    private static final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     public TempSubscription(Flow.Subscriber<? super TempInfo> subscriber, String town) {
         this.subscriber = subscriber;
@@ -14,14 +18,17 @@ public class TempSubscription implements Flow.Subscription {
 
     @Override
     public void request(long n) {
-        for (long i = 0L; i < n; i++) {
-            try {
-                subscriber.onNext(TempInfo.fetch(town));
-            } catch (Exception e) {
-                subscriber.onError(e);
-                break;
+        executor.submit( () -> {
+            for (long i = 0L; i < n; i++) {
+                try {
+                    subscriber.onNext(TempInfo.fetch(town));
+                } catch (Exception e) {
+                    subscriber.onError(e);
+                    break;
+                }
             }
-        }
+        });
+
     }
 
     @Override
